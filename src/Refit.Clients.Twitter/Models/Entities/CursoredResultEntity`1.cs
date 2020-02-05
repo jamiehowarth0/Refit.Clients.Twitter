687b962a0781ba2d4293a439d68e30e0fbe12cf0
Benjamin Howarth <iam@benjaminhowarth.com>
2020-02-05 00:54:52 +0000

    Replaced spaces

--- src/Refit.Clients.Twitter/Models/Entities/CursoredResultEntity`1.cs
+++ src/Refit.Clients.Twitter/Models/Entities/CursoredResultEntity`1.cs
@@ -1,12 +1,12 @@
 ﻿namespace Refit.Clients.Twitter.Models.Entities
 {
-    public abstract class CursoredResultEntity<T>
-        where T : class
-    {
-        public ulong NextCursor { get; set; }
-        public string NextCursorStr { get; set; }
-        public ulong PreviousCursor { get; set; }
-        public string PreviousCursorStr { get; set; }
-        public ulong? TotalCount { get; set; }
-    }
+	public abstract class CursoredResultEntity<T>
+		where T : class
+	{
+		public ulong NextCursor { get; set; }
+		public string NextCursorStr { get; set; }
+		public ulong PreviousCursor { get; set; }
+		public string PreviousCursorStr { get; set; }
+		public ulong? TotalCount { get; set; }
+	}
 }
