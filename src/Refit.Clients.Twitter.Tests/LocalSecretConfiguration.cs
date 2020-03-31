5606b21c0e7ab775fc459e0b4a55c9f2ae864582
Benjamin Howarth <me@benjaminhowarth.ltd>
2020-03-31 18:10:36 +0100

    Removed appsettings and recycled secrets

---
+++ src/Refit.Clients.Twitter.Tests/LocalSecretConfiguration.cs
@@ -0,0 +1,25 @@
+﻿// <copyright file="LocalSecretConfiguration.cs" company="Benjamin Howarth &amp; contributors">
+// Copyright (c) Benjamin Howarth &amp; contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.Extensions.Configuration;
+using NUnit.Framework;
+
+namespace Refit.Clients.Twitter.Tests
+{
+	[SetUpFixture]
+	public class LocalSecretConfiguration
+	{
+		[OneTimeSetUp]
+		public void LoadSecrets()
+		{
+			var config = new ConfigurationBuilder()
+				.AddJsonFile("appsettings.json")
+				.Build();
+        }
+	}
+}
